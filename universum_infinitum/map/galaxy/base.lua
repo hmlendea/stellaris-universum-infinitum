8d67758acad67517f2ec8ff1ba4332991210381d
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-01-03 05:10:10 +0200

    Reduced galaxy core radius

---
+++ universum_infinitum/map/galaxy/base.lua
@@ -0,0 +1,16 @@
+core_radius_perc = 0.15
+num_stars_core_perc = 0
+stars_min_dist = 10.0
+
+countries = {
+    ideal_sq_dist_between = 75*75,
+    min_sq_dist_between = 50*50,
+}
+
+fallen_empires = {
+    num = 5,
+    ideal_sq_dist_between = 150*150,
+    min_sq_dist_between = 100*100,
+    max_military_fleet_count = 5,
+    ships_per_fleet = 3,
+}
