df3552bdf09e93d7bef4f53774e47d444c08d04f
Mlendea Horatiu <Mlendea.Horatiu@GMail.com>
2017-01-14 18:07:06 +0200

    Greatly reduced fleet sizes & increased fleet costs and anomaly rates

---
+++ universum_infinitum/common/defines/ui_defines.lua
@@ -0,0 +1,9 @@
+NDefines.NGameplay.ANOMALY_SPAWN_CHANCE = 0.035
+
+NDefines.NGameplay.NAVY_SIZE_MAX				= 750
+NDefines.NGameplay.NAVY_SIZE_BASE 				= 5
+NDefines.NGameplay.NAVY_SIZE_POP_MULT			= 0.1
+NDefines.NGameplay.NAVY_SIZE_SPACEPORT_LEVEL	= { 1, 2, 3, 5, 6, 8 }
+
+NDefines.NShip.ENERGY_MAINTENANCE_MUL   = 0.009
+NDefines.NShip.MINERAL_MAINTENANCE_MUL  = 0.009
